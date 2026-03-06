8b0364848c927eafac80655c8c28ae8600c69a64
DanielLiuDL <liu00653@algonquinlive.com>
2026-03-06 16:02:01 -0500

    Set up basic communication framework between CODESYS and Arduino

--- Final_Project_Arduino.ino
+++ Final_Project_Arduino.ino
@@ -2,11 +2,33 @@
 Reads sensor inputs and outputs them to a PLC program.
 Then receives instructions from PLC to control a DC motor and robot arm.
 */
-enum MotorInstruction
+
+/*
+NOTES:
+- The following libraries need to be installed from the library manager
+  - Modbus-Arduino (NOT ArduinoModbus or Modbus)
+  - Modbus-Serial 
+- Don't use the serial communication pins (RX/TX), pin 0 and 1
+- The proximity sensors used have a normally HIGH logic level, for the sake of simplfying logic
+  the state of these sensors are inverted via convertDigitalInput()
+*/
+
+#include <ModbusSerial.h>
+
+const unsigned long BAUD = 115200; //Baud rate
+const byte ID = 1; //ID used for communications
+const int TXPIN = -1; //Normally disabled, used to control RX/TX pins when connected to a RS485 driver
+
+#define MySerial Serial //Defines serial port used
+
+ModbusSerial mb (MySerial, ID, TXPIN); //Defines modbus object
+
+enum class RegisterType //Determines type of register for each pin
 {
-  stop = 0,
-  moveCW = 1,
-  moveCCW = 2
+  ireg, //Analog input
+  hreg, //Analog output
+  ists, //Digital input
+  coil  //Digital output
 };
 
 enum RobotInstruction
@@ -29,215 +51,197 @@ enum RobotInstruction
   home = 15,
 };
 
+//Holds information for communication between CODESYS, Arduino IDE, and Arduino board
+struct Connection
+{
+  int pin; 
+  //Corresponding number to be entered into the map data in CODESYS
+  //Each registerType has it's own set of registerNum
+  int registerNum; 
+  //Corresponds to the channel that map data is to be entered in CODESYS
+  RegisterType registerType;
+};
+
 struct Sensor 
 {
-  int pin;
-  //NOTE the sensors used have normally HIGH outputs, when it senses something, it goes to LOW
+  Connection con;
   bool state;
 };
 
 struct Motor
 {
-  //Holds pins connected to L293D pins
-  int pin2A;
-  int pin1A;
-  int pinEN;
-  //Motor speed value between 0-255
-  int speed;
-  //direction = 1 is CW, direction = -1 is CCW
-  int direction;
-  int instruct;
+  //Holds connection data to L293D pins
+  Connection con2A;
+  Connection con1A;
+  Connection conEN;
+  int speed; //Motor speed value between 0-255
+  int direction; //direction = 1 is CW, direction = -1 is CCW
 };
 
 const int NUM_ROBOT_BITS = 4;
 struct Robot
 {
-  int inputPinBit[NUM_ROBOT_BITS];
-  int outputPinBit[NUM_ROBOT_BITS];
+  Connection inputConBit[NUM_ROBOT_BITS];
+  Connection outputConBit[NUM_ROBOT_BITS];
   int inputStateBit[NUM_ROBOT_BITS]; 
   int outputStateBit[NUM_ROBOT_BITS];
   int instruct;
 };
 
 //Initalize sensor objects
-Sensor sensorSprayer = {.pin = 2, .state = HIGH};
-Sensor sensorSqueegee = {.pin = 3, .state = HIGH};
-Sensor sensorCloth = {.pin = 4, .state = HIGH};
-Sensor sensorPosMin = {.pin = 5, .state = LOW};
-Sensor sensorPosMax = {.pin = 6, .state = LOW};
-Sensor sensorInfrared {.pin = 7, .state = LOW};
+Sensor sensorSprayer =  {.con = {.pin = 2, .registerNum = 0, .registerType = RegisterType::ists}, .state = HIGH};
+Sensor sensorSqueegee = {.con = {.pin = 3, .registerNum = 1, .registerType = RegisterType::ists}, .state = HIGH};
+Sensor sensorCloth =    {.con = {.pin = 4, .registerNum = 2, .registerType = RegisterType::ists}, .state = HIGH};
+Sensor sensorPosMin =   {.con = {.pin = 5, .registerNum = 3, .registerType = RegisterType::ists}, .state = LOW};
+Sensor sensorPosMax =   {.con = {.pin = 6, .registerNum = 4, .registerType = RegisterType::ists}, .state = LOW};
+Sensor sensorInfrared = {.con = {.pin = 7, .registerNum = 5, .registerType = RegisterType::ists}, .state = LOW};
 
 //Initialize motor object
-Motor motorWindow = {.pin2A = 7, .pin1A = 8, .pinEN = 9, .speed = 0, .direction = 1, .instruct = 0};
+Motor motorWindow = 
+{
+.con2A = {.pin = 8, .registerNum = 0, .registerType = RegisterType::coil}, 
+.con1A = {.pin = 9, .registerNum = 1, .registerType = RegisterType::coil}, 
+.conEN = {.pin = 10, .registerNum = 0, .registerType = RegisterType::hreg}, 
+.speed = 0, .direction = 1
+};
+
+//Initialize robot object
+Robot robotWindow = 
+{
+.inputConBit = {
+  {.pin = 11, .registerNum = 6, .registerType = RegisterType::ists},
+  {.pin = 12, .registerNum = 7, .registerType = RegisterType::ists},
+  {.pin = 13, .registerNum = 8, .registerType = RegisterType::ists},
+  {.pin = 14, .registerNum = 9, .registerType = RegisterType::ists}
+  }, 
+.outputConBit = {
+  {.pin = 15, .registerNum = 2, .registerType = RegisterType::coil},
+  {.pin = 16, .registerNum = 3, .registerType = RegisterType::coil},
+  {.pin = 17, .registerNum = 4, .registerType = RegisterType::coil},
+  {.pin = 18, .registerNum = 5, .registerType = RegisterType::coil}
+  }, 
+.inputStateBit = {LOW}, .outputStateBit = {LOW}, .instruct = 0
+};
+
 //Const speed for motor operation
 const int MOTOR_RUN_SPEED = 255;
 
 //////THESE ARE FOR TESTING
 unsigned long motorMillis = 0;
 const unsigned long MOTOR_DELAY = 700;
-
-unsigned long robotInstructMillis = 0;
-const unsigned long ROBOT_INSTRUCT_DELAY = 200;
 //////
 
-//Initialize robot object
-Robot robotWindow = {.inputPinBit = {10, 11, 12, 13}, .outputPinBit = {14, 15, 16, 17}, .inputStateBit = {LOW}, .outputStateBit = {LOW}, .instruct = 0};
-
+//Sets up pins and registers
+void setupConnection(Connection c);
 //Store input data
-void readInput();
-//Converts LOWs to HIGHs and HIGHs to LOWs
-//Used to set active sensor state output as HIGH, ad inactive sensor state output as LOW
-//because the sensor output is reversed
+void readDigitalInput();
+//Converts LOWs to HIGHs and HIGHs to LOWs, for the proximity sensor
 int convertDigitalInput(Sensor sensorObject);
-//Send stored sensor input data to PLC
-void sendOutputDataPLC();
-//Set states of motor's outputs
-void setOutputMotor();
-//Set states of robot's outputs
-void setOutputRobot();
 //Send state of motor's motion
 void sendOutputMotor();
 //Send state of robot's motion
 void sendOutputRobot();
 
 void setup() {
-  pinMode(sensorSprayer.pin, INPUT);
-  pinMode(sensorSqueegee.pin, INPUT);
-  pinMode(sensorCloth.pin, INPUT);
-  pinMode(sensorPosMin.pin, INPUT);
-  pinMode(sensorPosMax.pin, INPUT);
-  pinMode(sensorInfrared.pin, INPUT);
-  pinMode(motorWindow.pin2A, OUTPUT);
-  pinMode(motorWindow.pin1A, OUTPUT);
-  pinMode(motorWindow.pinEN, OUTPUT);
+  //Set up baud rate
+  MySerial.begin(BAUD);
+  mb.config(BAUD);
 
+  //Sets up pins and registers
+  setupConnection(sensorSprayer.con);
+  setupConnection(sensorSqueegee.con);
+  setupConnection(sensorCloth.con);
+  setupConnection(sensorPosMin.con);
+  setupConnection(sensorPosMax.con);
+  setupConnection(sensorInfrared.con);
+  setupConnection(motorWindow.con2A);
+  setupConnection(motorWindow.con1A);
+  setupConnection(motorWindow.conEN);
   for (int i = 0; i < NUM_ROBOT_BITS; i++)
   {
-    pinMode(robotWindow.inputPinBit[i], INPUT);
-    pinMode(robotWindow.outputPinBit[i], OUTPUT);
+    setupConnection(robotWindow.inputConBit[i]);
+    setupConnection(robotWindow.outputConBit[i]);
   }
 }
 
 void loop() {
-  readInput();
-  setOutputMotor();
+  //Task that performs all operations on MODBUS
+  mb.task(); 
+
+  readDigitalInput();
   sendOutputMotor();
-  setOutputRobot();
   sendOutputRobot();
 }
 
-void readInput()
+void setupConnection(Connection c)
 {
+  if (c.registerType == RegisterType::ists) //Digital input
+  {
+    pinMode(c.pin, INPUT);
+    mb.addIsts(c.registerNum);
+  }
+  if (c.registerType == RegisterType::coil) //Digital output
+  {
+    pinMode(c.pin, OUTPUT);
+    mb.addCoil(c.registerNum);
+  }
+  if (c.registerType == RegisterType::ireg) //Analog input
+  {
+    pinMode(c.pin, INPUT);
+    mb.addIreg(c.registerNum);
+  }
+  if (c.registerType == RegisterType::hreg) //Analog output
+  {
+    pinMode(c.pin, OUTPUT);
+    mb.addHreg(c.registerNum);
+  }
+}
+
+void readDigitalInput()
+{
+  //Stores input state values
   sensorSprayer.state = convertDigitalInput(sensorSprayer);
   sensorSqueegee.state = convertDigitalInput(sensorSqueegee);
   sensorCloth.state = convertDigitalInput(sensorCloth);
   sensorPosMin.state = convertDigitalInput(sensorPosMin);
   sensorPosMax.state = convertDigitalInput(sensorPosMax);
+  sensorInfrared.state = digitalRead(sensorInfrared.con.pin);
 
-  sensorInfrared.state = digitalRead(sensorInfrared.pin);
-  
+  //Sets states of the digital inputs registers for CODESYS
+  mb.Ists(sensorSprayer.con.registerNum, sensorSprayer.state);
+  mb.Ists(sensorSqueegee.con.registerNum, sensorSqueegee.state);
+  mb.Ists(sensorCloth.con.registerNum, sensorCloth.state);
+  mb.Ists(sensorPosMin.con.registerNum, sensorPosMin.state);
+  mb.Ists(sensorPosMax.con.registerNum, sensorPosMax.state);
+  mb.Ists(sensorInfrared.con.registerNum, sensorInfrared.state);
+
   for (int i = 0; i < NUM_ROBOT_BITS; i++)
   {
-    robotWindow.inputStateBit[i] = digitalRead(robotWindow.inputPinBit[i]);
+    robotWindow.inputStateBit[i] = digitalRead(robotWindow.inputConBit[i].pin);
+    mb.Ists(robotWindow.inputConBit[i].registerNum, robotWindow.inputStateBit[i]);
   }
 }
 
 int convertDigitalInput(Sensor sensorObject)
 {
-  int state = digitalRead(sensorObject.pin) == HIGH ? LOW : HIGH;
+  int state = digitalRead(sensorObject.con.pin) == HIGH ? LOW : HIGH;
   return state;
 }
 
-void setOutputMotor()
+void sendOutputRobot()
 {
-  if (sensorPosMin.state == HIGH && sensorPosMax.state == HIGH)
-  {
-    motorWindow.speed = 0;
-  }
-  //Min sensor is active
-  else if (sensorPosMin.state == HIGH)
-  {
-    motorWindow.speed = MOTOR_RUN_SPEED;
-    if (motorWindow.direction == -1)
-    {
-      motorMillis = millis();
-    }
-    motorWindow.direction = 1;
-    
-  }
-  //Max sensor is active
-  else if (sensorPosMax.state == HIGH)
-  {
-    motorWindow.speed = MOTOR_RUN_SPEED;
-    if (motorWindow.direction == 1)
-    {
-      motorMillis = millis();
-    }
-    motorWindow.direction = -1;
-  }
-  //No sensors are active
-  else
-  {
-    motorWindow.speed = MOTOR_RUN_SPEED;
-  }
-}
-
-void setOutputRobot()
-{
-  /////THIS IS FOR TESTING
-  if (millis() - robotInstructMillis > ROBOT_INSTRUCT_DELAY)
-  {
-    robotInstructMillis = millis();
-
-    if (robotWindow.instruct >= 15)
-    {
-      robotWindow.instruct = 0;
-    }
-    else
-    {
-      robotWindow.instruct = robotWindow.instruct + 1;
-    }
-  }
-  /////
-  
-  //Converts integer value of robot's .instruct into 4-bit binary value
+  robotWindow.instruct = 0;
   for (int i = 0; i < NUM_ROBOT_BITS; i++)
   {
-    //Performs a Bitwise AND operation on each individual bit of the 4-bit robot instruction number
-    robotWindow.outputStateBit[i] = (robotWindow.instruct & (1<<i)) == (1<<i) ? HIGH : LOW;
+    robotWindow.outputStateBit[i] = mb.Coil(robotWindow.outputConBit[i].registerNum);
+    digitalWrite(robotWindow.outputConBit[i].pin, robotWindow.outputStateBit[i]);
+    robotWindow.instruct = robotWindow.outputStateBit[i] == true ? robotWindow.instruct + (1<<i) : robotWindow.instruct;
   }
 }
 
 void sendOutputMotor()
 {
-  //////For testing purposes
-  if (millis() - motorMillis > MOTOR_DELAY)
-  {
-    if (motorWindow.direction == 1)
-    {
-      digitalWrite(motorWindow.pin2A, HIGH);
-      digitalWrite(motorWindow.pin1A, LOW);
-    }
-    else if (motorWindow.direction == -1)
-    {
-      digitalWrite(motorWindow.pin2A, LOW);
-      digitalWrite(motorWindow.pin1A, HIGH);
-    }
-    //Set fan speed
-    analogWrite(motorWindow.pinEN, motorWindow.speed);
-  }
-  else
-  {
-    digitalWrite(motorWindow.pin2A, LOW);
-    digitalWrite(motorWindow.pin1A, LOW);
-  }
-  //////
-}
-
-void sendOutputRobot()
-{
-  for (int i = 0; i < NUM_ROBOT_BITS; i++)
-  {
-    digitalWrite(robotWindow.outputPinBit[i], robotWindow.outputStateBit[i]);
-  }
+  analogWrite(motorWindow.conEN.pin, mb.Hreg(motorWindow.conEN.registerNum));
+  digitalWrite(motorWindow.con2A.pin, mb.Coil(motorWindow.con2A.registerNum));
+  digitalWrite(motorWindow.con1A.pin, mb.Coil(motorWindow.con1A.registerNum));
 }
